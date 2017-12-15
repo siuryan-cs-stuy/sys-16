68f2b541eeec6ab351841cd309e2bb5bebd094b5
Ryan Siu <rsiu@stuy.edu>
2017-12-15 14:56:42 -0500

    Reformatting

--- basic_server.c
+++ basic_server.c
@@ -4,30 +4,30 @@ void camelCase(char *);
 
 int main() {
 
-  int to_client;
-  int from_client;
+    int to_client;
+    int from_client;
 
-  from_client = server_handshake( &to_client );
+    from_client = server_handshake( &to_client );
 
-  while(1) {
-    char message[256];
-    read(from_client, message, sizeof(message));
-    printf("[client] %s\n", message);
+    while(1) {
+        char message[256];
+        read(from_client, message, sizeof(message));
+        printf("[client] %s\n", message);
 
-    camelCase(message);
-    write(to_client, message, sizeof(message));
-  }
+        camelCase(message);
+        write(to_client, message, sizeof(message));
+    }
 
-  return 0;
+    return 0;
 }
 
 void camelCase(char *message) {
-  unsigned int i;
-  for (i = 0; i < strlen(message); i++) {
-    if (i % 2 == 0) {
-      message[i] = toupper(message[i]);
-    } else {
-      message[i] = tolower(message[i]);
+    unsigned int i;
+    for (i = 0; i < strlen(message); i++) {
+        if (i % 2 == 0) {
+            message[i] = toupper(message[i]);
+        } else {
+            message[i] = tolower(message[i]);
+        }
     }
-  }
 }
--- pipe_networking.c
+++ pipe_networking.c
@@ -12,34 +12,34 @@
   =========================*/
 int server_handshake(int *to_client) {
 
-  //Step 1.1
-  mkfifo("wkp", 0644);
-  printf("[server] created wkp\n");
-  int wkp = open("wkp", O_RDONLY);
-  printf("[server] opened wkp\n");
-  char pipe[64];
+    //Step 1.1
+    mkfifo("wkp", 0644);
+    printf("[server] created wkp\n");
+    int wkp = open("wkp", O_RDONLY);
+    printf("[server] opened wkp\n");
+    char pipe[64];
 
-  //Step 2.3
-  read(wkp, &pipe, sizeof(pipe));
-  printf("[server] pipe: %s\n", pipe);
-  remove("wkp");
-  printf("[server] WKP removed\n");
+    //Step 2.3
+    read(wkp, &pipe, sizeof(pipe));
+    printf("[server] pipe: %s\n", pipe);
+    remove("wkp");
+    printf("[server] WKP removed\n");
 
-  //Step 2.4
-  *to_client = open(pipe, O_WRONLY, 0644);
-  printf("[server] writing %s\n", ACK);
-  write(*to_client, ACK, sizeof(int));
+    //Step 2.4
+    *to_client = open(pipe, O_WRONLY, 0644);
+    printf("[server] writing %s\n", ACK);
+    write(*to_client, ACK, sizeof(int));
 
-  char response[256];
-  int err;
-  err = read(wkp, response, sizeof(response));
-  to_client = &wkp;
-  if (err == -1) {
-    printf("[server] Error: didn't receive client message\n");
-    return 1;
-  }
-  printf("[server] read %s\n", response);
-  return wkp;
+    char response[256];
+    int err;
+    err = read(wkp, response, sizeof(response));
+    to_client = &wkp;
+    if (err == -1) {
+        printf("[server] Error: didn't receive client message\n");
+        return 1;
+    }
+    printf("[server] read %s\n", response);
+    return wkp;
 }
 
 
@@ -53,39 +53,39 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int pid = getpid();
-  char pipe[64];
-  sprintf(pipe, "%d", pid);
+    int pid = getpid();
+    char pipe[64];
+    sprintf(pipe, "%d", pid);
 
-  //Step 1.2
-  mkfifo(pipe, 0644);
-  printf("[client] made pipe %s\n", pipe);
+    //Step 1.2
+    mkfifo(pipe, 0644);
+    printf("[client] made pipe %s\n", pipe);
 
-  //Step 2.1
-  *to_server = open("wkp", O_WRONLY, 0644);
-  printf("[client] opened wkp\n");
-  printf("[client] writing %s\n", pipe);
-  write(*to_server, &pipe, sizeof(pipe));
-  //Step 2.2
+    //Step 2.1
+    *to_server = open("wkp", O_WRONLY, 0644);
+    printf("[client] opened wkp\n");
+    printf("[client] writing %s\n", pipe);
+    write(*to_server, &pipe, sizeof(pipe));
+    //Step 2.2
 
-  //Step 2.4
-  int priv = open(pipe, O_RDONLY, 0644);
-  printf("[client] opened pipe %s\n", pipe);
+    //Step 2.4
+    int private = open(pipe, O_RDONLY, 0644);
+    printf("[client] opened pipe %s\n", pipe);
 
-  int res = -1;
-  read(priv, &res, HANDSHAKE_BUFFER_SIZE);
-  if (res == -1) {
-    printf("[client] Error: didn't receive server message\n");
-    return 1;
+    int res = -1;
+    read(private, &res, HANDSHAKE_BUFFER_SIZE);
+    if (res == -1) {
+        printf("[client] Error: didn't receive server message\n");
+        return 1;
 
-  }
-  //Step 2.5?
-  printf("[client] read %d\n", res);
-  remove(pipe);
-  printf("[client] removed %s\n", pipe);
+    }
+    //Step 2.5?
+    printf("[client] read %d\n", res);
+    remove(pipe);
+    printf("[client] removed %s\n", pipe);
 
-  int msg = 0;
-  printf("[client] writing %d\n", msg);
-  write(*to_server, &msg, sizeof(int));
-  return priv;
+    int msg = 0;
+    printf("[client] writing %d\n", msg);
+    write(*to_server, &msg, sizeof(int));
+    return private;
 }
