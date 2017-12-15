db17d5ec13506a4c30fac207912b298a5694726b
Ryan Siu <rsiu@stuy.edu>
2017-12-15 15:27:13 -0500

    Change handshake ordering

--- pipe_networking.c
+++ pipe_networking.c
@@ -17,7 +17,7 @@ int server_handshake(int *to_client) {
     printf("[server] created wkp\n");
     int wkp = open("wkp", O_RDONLY);
     printf("[server] opened wkp\n");
-    char pipe[64];
+    char pipe[HANDSHAKE_BUFFER_SIZE];
 
     //Step 2.3
     read(wkp, &pipe, sizeof(pipe));
@@ -30,7 +30,7 @@ int server_handshake(int *to_client) {
     printf("[server] writing %s\n", ACK);
     write(*to_client, ACK, sizeof(int));
 
-    char response[256];
+    char response[HANDSHAKE_BUFFER_SIZE];
     int err;
     err = read(wkp, response, sizeof(response));
     to_client = &wkp;
@@ -55,27 +55,32 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
     int pid = getpid();
-    char pipe[64];
+    char pipe[HANDSHAKE_BUFFER_SIZE];
     sprintf(pipe, "%d", pid);
+
+    //Step 2.1
+    *to_server = open("wkp", O_WRONLY, 0644);
+    if (*to_server == -1) {
+        exit(1);
+    }
+    printf("[client] opened wkp\n");
 
     //Step 1.2
     mkfifo(pipe, 0644);
     printf("[client] made pipe %s\n", pipe);
 
-    //Step 2.1
-    *to_server = open("wkp", O_WRONLY, 0644);
-    printf("[client] opened wkp\n");
+    //Step 2.2
     printf("[client] writing %s\n", pipe);
     write(*to_server, &pipe, sizeof(pipe));
-    //Step 2.2
 
     //Step 2.4
     int private = open(pipe, O_RDONLY, 0644);
     printf("[client] opened pipe %s\n", pipe);
 
-    int res = -1;
-    read(private, &res, HANDSHAKE_BUFFER_SIZE);
-    if (res == -1) {
+    int res;
+    int err;
+    err = read(private, &res, HANDSHAKE_BUFFER_SIZE);
+    if (err == -1) {
         printf("[client] Error: didn't receive server message\n");
         return 1;
 
@@ -85,9 +90,8 @@ int client_handshake(int *to_server) {
     remove(pipe);
     printf("[client] removed %s\n", pipe);
 
-    int msg = 0;
-    printf("[client] writing %d\n", msg);
-    write(*to_server, &msg, sizeof(int));
+    printf("[client] writing %s\n", ACK);
+    write(*to_server, ACK, sizeof(ACK));
     printf("[client] connected to server\n");
     return private;
 }
